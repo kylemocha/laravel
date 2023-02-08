d065fe2900e582a9be27222fd177af04b7008413
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-02-08 12:15:28 +0800

    fixes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::view('/', 'welcome');
 //Route::view('/main_home', 'main_home'); 
 //Route::view('/login', 'login'); 
 //Route::view('/register', 'register'); 
-Route::view('/home_user', 'home_user'); 
+//Route::view('/home_user', 'home_user'); 
 
 Route::get('main_home', function () {
     return view('layout/main_home');
@@ -18,6 +18,9 @@ Route::get('login', function () {
 });
 Route::get('register', function () {
     return view('layout/register');
+});
+Route::get('home_user', function () {
+    return view('home_user');
 });
 
 
