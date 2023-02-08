0ee975130b88583c2df470f9239cda37a722805c
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-02-08 08:33:01 +0800

    fixes

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\CustomAuthController;
 
 Route::view('/', 'welcome'); 
 //Route::view('/main_home', 'main_home'); 
-Route::view('/login', 'login'); 
+//Route::view('/login', 'login'); 
 Route::view('/register', 'register'); 
 Route::view('/home_user', 'home_user'); 
 
 Route::get('main_home', function () {
     return view('layout/main_home');
+});
+
+Route::get('login', function () {
+    return view('layout/login');
 });
 
 
