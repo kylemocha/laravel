2e3dc544e50e6102f0faa83151cec888a8de4646
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-02-15 13:09:28 +0800

    fixes

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -6,25 +6,12 @@
     <link href="https://fonts.googleapis.com/css?family=Assistant:400,700" rel="stylesheet">
     <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
     <script src="https://code.jquery.com/jquery.js"></script>
-    <script>
-    function show(elementID) {
-    var ele = document.getElementById(elementID);
-    if (!ele) {
-        alert("no such element");
-        return;
-    }
-    var pages = document.getElementsByClassName('page');
-    for(var i = 0; i < pages.length; i++) {
-        pages[i].style.display = 'none';
-    }
-    ele.style.display = 'block';
-    }
-    </script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>   
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/hom.css" rel="stylesheet" type="text/css">
+    <link href="js/home.js" rel="stylesheet" type="text/css">
     <title>Home Page</title>
 </head>
 
