9a299a7a4b989e842a3ed03c677102857e311ce8
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-02-15 13:17:25 +0800

    fixes

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -9,9 +9,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>   
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <script>
+      function show(elementID) {
+    var ele = document.getElementById(elementID);
+    if (!ele) {
+        alert("no such element");
+        return;
+    }
+    var pages = document.getElementsByClassName('page');
+    for(var i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+    ele.style.display = 'block';
+    }
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/hom.css" rel="stylesheet" type="text/css">
-    <link href="js/home.js" rel="stylesheet" type="text/css">
     <title>Home Page</title>
 </head>
 
