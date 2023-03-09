a13945f23d463c43b8bf734cb0a60e0d4200454a
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-03-09 13:03:03 +0800

    fixes

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -304,7 +304,6 @@
             </div>
           </header>
        
-
         <div class="row justify-content-center mb-3">
           <div class="col-md-12">
             <div class="card shadow-0 border rounded-3">
@@ -337,6 +336,39 @@
             </div>
           </div>
         </div>
+        <div class="row justify-content-center mb-3">
+          <div class="col-md-12">
+            <div class="card shadow-0 border rounded-3">
+              <div class="card-body">
+                <div class="row g-0">
+                  <div class="col-xl-3 col-md-4 d-flex justify-content-center">
+                    <div class="bg-image hover-zoom ripple rounded ripple-surface me-md-3 mb-3 mb-md-0">
+                      <img src="https://i.ibb.co/VV1DxHf/3173-8-diary-icon-iconbunny.jpg" alt="3173-8-diary-icon-iconbunny" width="100" height="130" class="w-100"/>
+                      <a href="#!">
+                        <div class="hover-overlay">
+                          <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-md-5 col-sm-7">
+                    <h5>Day 2</h5>
+                    <p class="text mb-4 mb-md-0">
+                      Short description
+                    </p>
+                  </div>
+                  <div class="col-xl-3 col-md-3 col-sm-5">
+                    <div class="mt-4">
+                      <button class="btn btn-primary shadow-0" type="button">Open</button>
+                      <a href="#!" class="btn btn-light border px-2 pt-2 icon-hover"><i class="fas fa-heart fa-lg px-1"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
 
     <div class="page" id="page4" style="display:none">
         <h2 style="font-weight: bold;">Rate your Mood</h2>
