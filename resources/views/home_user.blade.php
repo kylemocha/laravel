a7190926186a8710ee6e20d951b2ca4ca54f77d4
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-03-24 13:50:27 +0800

    fixes

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -161,11 +161,90 @@
                               <!-- Modal content -->
                               <div class="modal-content2">
                               <span class="close1">&times;</span>
-                               <p>Some text in the Modal..</p>
+                              <div class="formbold-main-wrapper">
+                                <div class="formbold-form-wrapper">
+                                  <form action="#">
+                                    <div class="formbold-mb-5">
+                                      <label for="name" class="formbold-form-label"> Full Name </label>
+                                      <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        placeholder="Full Name"
+                                        class="formbold-form-input"
+                                      />
+                                    </div>
+                                    <div class="formbold-mb-5">
+                                      <label for="phone" class="formbold-form-label"> Phone Number </label>
+                                      <input
+                                        type="text"
+                                        name="phone"
+                                        id="phone"
+                                        placeholder="Enter your phone number"
+                                        class="formbold-form-input"
+                                      />
+                                    </div>
+                                    <div class="formbold-mb-5">
+                                      <label for="email" class="formbold-form-label"> Email Address </label>
+                                      <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        placeholder="Enter your email"
+                                        class="formbold-form-input"
+                                      />
+                                    </div>
+                                    <div class="flex flex-wrap formbold--mx-3">
+                                      <div class="w-full sm:w-half formbold-px-3">
+                                        <div class="formbold-mb-5 w-full">
+                                          <label for="date" class="formbold-form-label"> Date </label>
+                                          <input
+                                            type="date"
+                                            name="date"
+                                            id="date"
+                                            class="formbold-form-input"
+                                          />
+                                        </div>
+                                      </div>
+                                      <div class="w-full sm:w-half formbold-px-3">
+                                        <div class="formbold-mb-5">
+                                          <label for="time" class="formbold-form-label">Time </label>
+                                          <input
+                                            type="time"
+                                            name="time"
+                                            id="time"
+                                            class="formbold-form-input"
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                              
+                                    <div class="formbold-mb-5 formbold-pt-3">
+                                      <label class="formbold-form-label formbold-form-label-2">
+                                        Address Details
+                                      </label>
+                                      <div class="flex flex-wrap formbold--mx-3">
+                                        <div class="w-full sm:w-half formbold-px-3">
+                                          <div class="formbold-mb-5">
+                                            <input
+                                              type="text"
+                                              name="area"
+                                              id="area"
+                                              placeholder="Enter address"
+                                              class="formbold-form-input"
+                                            />
+                                          </div>
+                                        </div>      
+                                      </div>
+                                    </div>
+                                    <div>
+                                      <button class="formbold-btn">Book Appointment</button>
+                                    </div>
+                                  </form>
+                                </div>
                               </div>
-
                               </div>
-
+                              </div>
                           </div>
                       </div>
                   </div>
@@ -230,7 +309,7 @@
                   <a href="#"><img class="card-img-top" src="https://i.ibb.co/mH6ty5W/get-the-most-out-of-mental-health-therapy.jpg" alt="wrappixel kit"></a>
                   <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Mar<span class="d-block">23</span></div>
                   <h5 class="font-weight-medium mt-3"><a href="#" class="text-decoration-none link">Session with Dr. Ester Dean</a></h5>
-                  <p class="mt-3">CDOC</p>
+                  <p class="mt-3"> CDOC</p>
                   <!--<a href="#" class="text-decoration-none linking text-themecolor mt-2">Learn More</a>-->
                 </div>
               </div>
@@ -240,7 +319,7 @@
                   <a href="#"><img class="card-img-top" src="https://i.ibb.co/mH6ty5W/get-the-most-out-of-mental-health-therapy.jpg" alt="wrappixel kit"></a>
                   <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Mar<span class="d-block">23</span></div>
                   <h5 class="font-weight-medium mt-3"><a href="#" class="text-decoration-none link">Session with Dr. Ester Dean</a></h5>
-                  <p class="mt-3">CDOC</p>
+                  <p class="mt-3"> CDOC</p>
                   <!--<a href="#" class="text-decoration-none linking text-themecolor mt-2">Learn More</a>-->
                 </div>
               </div>
@@ -250,7 +329,7 @@
                   <a href="#"><img class="card-img-top" src="https://i.ibb.co/mH6ty5W/get-the-most-out-of-mental-health-therapy.jpg" alt="wrappixel kit"></a>
                   <div class="date-pos bg-info-gradiant p-2 d-inline-block text-center rounded text-white position-absolute">Mar<span class="d-block">23</span></div>
                   <h5 class="font-weight-medium mt-3"><a href="#" class="text-decoration-none link">Session with Dr. Ester Dean</a></h5>
-                  <p class="mt-3">CDOC</p>
+                  <p class="mt-3"> CDOC</p>
                   <!--<a href="#" class="text-decoration-none linking text-themecolor mt-2">Learn More</a>-->
                 </div>
               </div>
