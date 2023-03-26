d0f619f26d29ea715d6940fc550a292a782241f7
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-03-27 07:41:51 +0800

    fixes

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -283,7 +283,103 @@
                             <!-- Modal content -->
                             <div class="modal-content3">
                              <span class="close2">&times;</span>
-                             <p>Some text in the Modal..</p>
+                             <div class="formbold-main-wrapper">
+                              <div class="formbold-form-wrapper">
+                                  <!---->
+                                    <h4 style="font-weight: bold;">See Dr. ---- schedule</h4>
+                                    <div class="calendar"></div> 
+                                  
+                                <form action="#">
+                                  <div class="formbold-mb-5">
+                                    <label for="name" class="formbold-form-label" style="font-weight: bold;"> Full Name </label>
+                                    <input
+                                      type="text"
+                                      name="name"
+                                      id="name"
+                                      placeholder="Full Name"
+                                      class="formbold-form-input"
+                                    />
+                                  </div>
+                                  <div class="formbold-mb-5">
+                                    <label for="phone" class="formbold-form-label" style="font-weight: bold;"> Phone Number </label>
+                                    <input
+                                      type="text"
+                                      name="phone"
+                                      id="phone"
+                                      placeholder="Enter your phone number"
+                                      class="formbold-form-input"
+                                    />
+                                  </div>
+                                  <div class="formbold-mb-5">
+                                    <label for="email" class="formbold-form-label" style="font-weight: bold;"> Email Address </label>
+                                    <input
+                                      type="email"
+                                      name="email"
+                                      id="email"
+                                      placeholder="Enter your email"
+                                      class="formbold-form-input"
+                                    />
+                                  </div>
+                                  <div class="flex flex-wrap formbold--mx-3">
+                                    <div class="w-full sm:w-half formbold-px-3">
+                                      <div class="formbold-mb-5 w-full">
+                                        <label for="date" class="formbold-form-label" style="font-weight: bold;"> Date </label>
+                                        <input
+                                          type="date"
+                                          name="date"
+                                          id="date"
+                                          class="formbold-form-input"
+                                        />
+                                      </div>
+                                    </div>
+                                    <div class="w-full sm:w-half formbold-px-3">
+                                      <div class="formbold-mb-5">
+                                        <label for="time" class="formbold-form-label">Time </label>
+                                        <input
+                                          type="time"
+                                          name="time"
+                                          id="time"
+                                          class="formbold-form-input"
+                                        />
+                                      </div>
+                                    </div>
+                                  </div>
+                            
+                                  <div class="formbold-mb-5 formbold-pt-3">
+                                    <label class="formbold-form-label formbold-form-label-2" style="font-weight: bold;">
+                                      Address
+                                    </label>
+                                    <div class="flex flex-wrap formbold--mx-3">
+                                      <div class="w-full sm:w-half formbold-px-3">
+                                        <div class="formbold-mb-5">
+                                          <input
+                                            type="text"
+                                            name="area"
+                                            id="area"
+                                            placeholder="Enter area"
+                                            class="formbold-form-input"
+                                          />
+                                        </div>
+                                      </div>   
+                                      <div class="w-full sm:w-half formbold-px-3">
+                                        <div class="formbold-mb-5">
+                                          <input
+                                            type="text"
+                                            name="city"
+                                            id="city"
+                                            placeholder="Enter city"
+                                            class="formbold-form-input"
+                                          />
+                                        </div>
+                                      </div>   
+                                    </div>
+                                  </div>
+                                  <div>
+                                    <button class="formbold-btn" style="font-size: 21px;">Book Appointment</button>
+                                  </div>
+                                </form>
+                              </div>
+                            </div>
                             </div>
                             </div>
 
@@ -310,7 +406,103 @@
                             <!-- Modal content -->
                             <div class="modal-content4">
                              <span class="close3">&times;</span>
-                             <p>Some text in the Modal..</p>
+                             <div class="formbold-main-wrapper">
+                              <div class="formbold-form-wrapper">
+                                  <!---->
+                                    <h4 style="font-weight: bold;">See Dr. ---- schedule</h4>
+                                    <div class="calendar"></div> 
+                                  
+                                <form action="#">
+                                  <div class="formbold-mb-5">
+                                    <label for="name" class="formbold-form-label" style="font-weight: bold;"> Full Name </label>
+                                    <input
+                                      type="text"
+                                      name="name"
+                                      id="name"
+                                      placeholder="Full Name"
+                                      class="formbold-form-input"
+                                    />
+                                  </div>
+                                  <div class="formbold-mb-5">
+                                    <label for="phone" class="formbold-form-label" style="font-weight: bold;"> Phone Number </label>
+                                    <input
+                                      type="text"
+                                      name="phone"
+                                      id="phone"
+                                      placeholder="Enter your phone number"
+                                      class="formbold-form-input"
+                                    />
+                                  </div>
+                                  <div class="formbold-mb-5">
+                                    <label for="email" class="formbold-form-label" style="font-weight: bold;"> Email Address </label>
+                                    <input
+                                      type="email"
+                                      name="email"
+                                      id="email"
+                                      placeholder="Enter your email"
+                                      class="formbold-form-input"
+                                    />
+                                  </div>
+                                  <div class="flex flex-wrap formbold--mx-3">
+                                    <div class="w-full sm:w-half formbold-px-3">
+                                      <div class="formbold-mb-5 w-full">
+                                        <label for="date" class="formbold-form-label" style="font-weight: bold;"> Date </label>
+                                        <input
+                                          type="date"
+                                          name="date"
+                                          id="date"
+                                          class="formbold-form-input"
+                                        />
+                                      </div>
+                                    </div>
+                                    <div class="w-full sm:w-half formbold-px-3">
+                                      <div class="formbold-mb-5">
+                                        <label for="time" class="formbold-form-label">Time </label>
+                                        <input
+                                          type="time"
+                                          name="time"
+                                          id="time"
+                                          class="formbold-form-input"
+                                        />
+                                      </div>
+                                    </div>
+                                  </div>
+                            
+                                  <div class="formbold-mb-5 formbold-pt-3">
+                                    <label class="formbold-form-label formbold-form-label-2" style="font-weight: bold;">
+                                      Address
+                                    </label>
+                                    <div class="flex flex-wrap formbold--mx-3">
+                                      <div class="w-full sm:w-half formbold-px-3">
+                                        <div class="formbold-mb-5">
+                                          <input
+                                            type="text"
+                                            name="area"
+                                            id="area"
+                                            placeholder="Enter area"
+                                            class="formbold-form-input"
+                                          />
+                                        </div>
+                                      </div>   
+                                      <div class="w-full sm:w-half formbold-px-3">
+                                        <div class="formbold-mb-5">
+                                          <input
+                                            type="text"
+                                            name="city"
+                                            id="city"
+                                            placeholder="Enter city"
+                                            class="formbold-form-input"
+                                          />
+                                        </div>
+                                      </div>   
+                                    </div>
+                                  </div>
+                                  <div>
+                                    <button class="formbold-btn" style="font-size: 21px;">Book Appointment</button>
+                                  </div>
+                                </form>
+                              </div>
+                            </div>
                             </div>
                           </div>
 
