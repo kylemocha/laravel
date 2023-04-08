5dac81582e1c4fc04c867e64ace957e1530f1b8c
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-08 15:00:49 +0800

    need to fix some configurations

--- app/Http/Controllers/JournalViewController.php
+++ app/Http/Controllers/JournalViewController.php
@@ -10,15 +10,8 @@ use App\Http\Controllers\Controller;
 class JournalViewController extends Controller
 {
     public function index(){
-        //$journal_details = DB::table('journal_details')->select('id','User_name','JournalDate')->get();
-        //return view('home_user') ->with('journal_details', $journal_details);
-        //return view('home_user', compact('journal_details'));
-        //return view('home_user')->with('journal_details', $journal_details);
         $js = DB::select('select * from journal_details');
-       // return view('journal_details',['js'=>$js]);
-        //return view('/home_user', compact('js'));
-        //return view('home_user')->with('home_user', $js);
-        return view('home_user',compact('js'));
+        return view ('index')->with('User_name',$js);
     }    
     
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\ContactUsFormController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\JournalController;
 use App\Http\Controllers\JournalViewController;
-use App\Http\Controllers\RatingsController;
 
 
 Route::view('/', 'welcome'); 
@@ -32,7 +31,7 @@ Route::get('home_user', function () {
 Route::get('/insert','App\Http\Controllers\JournalController@insertform');
 Route::post('/create','JournalController@insert');
 //retrive data
-Route::get('view-records','JournalViewController@index');
+Route::get('/','JournalViewController@index');
 
 //Route::get('/insert','App\Http\Controllers\RatingsController@insertForm');
 //Route::post('/create1','JournalController@insert');
