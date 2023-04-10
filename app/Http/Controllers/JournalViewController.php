8202ae2ba0019338e9058c1218ef75e57bd5ae8b
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-10 10:35:03 +0800

    ambot gikapoy nako

--- app/Http/Controllers/JournalViewController.php
+++ app/Http/Controllers/JournalViewController.php
@@ -11,12 +11,14 @@ use App\Models\JournalView;
 class JournalViewController extends Controller
 {
     public function get_users(){
-        //$user = DB::table('journal_details')->select(Auth::user()->id,'User_name','message') ->get();
+        $users = DB::table('journal_details')->select(Auth::user()->id,'User_name','message') ->get();
         //$users=JournalView::all();
         //return view('home_user',$users);
-         $users['users'] =JournalView::orderBy('User_name','message')->get();
+         //$users['users'] =JournalView::orderBy('User_name','message')->get();
+         //$users = DB::table('journal_details')->get()->toArray();
+         return view('home_users')->with('users', $users);
 
-         return view("home_user",['users'=>$users]);
+         //return view("home_user",['users'=>$users]);
          //return view('home_user', compact('users'));
          //return view('home_user', $users);
     }    
