d1bfeb3998e8ded713073bd27dabc09347747a70
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-10 11:47:56 +0800

    a

--- app/Http/Controllers/JournalViewController.php
+++ app/Http/Controllers/JournalViewController.php
@@ -11,16 +11,18 @@ use App\Models\JournalView;
 class JournalViewController extends Controller
 {
     public function get_users(){
-        $users = DB::table('journal_details')->select(Auth::user()->id,'User_name','message') ->get();
-        //$users=JournalView::all();
+        //$users = DB::table('journal_details')->select(Auth::user()->id,'User_name','message') ->get();
+        $users=JournalView::all();
         //return view('home_user',$users);
          //$users['users'] =JournalView::orderBy('User_name','message')->get();
          //$users = DB::table('journal_details')->get()->toArray();
-         return view('home_users')->with('users', $users);
-
+        return view('home_user')->with($users);
+         
          //return view("home_user",['users'=>$users]);
-         //return view('home_user', compact('users'));
+        // return view('home_user', compact('users'));
          //return view('home_user', $users);
+         //$users = DB::select('select * from journal_details');
+         //sreturn view('home_user',['users'=>$users]);
     }    
     
 }
--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -860,12 +860,11 @@
                     </div>
                   </div>
                   <!--error here-->
-                  <div class="col-xl-6 col-md-5 col-sm-7">
+                  <!--<div class="col-xl-6 col-md-5 col-sm-7">
                     <h5>Day 1</h5>
                     <p class="text mb-4 mb-md-0">Description</p>
-                  </div>
-                  
-                  
+                  </div>-->
+                 
                   <div class="col-xl-3 col-md-3 col-sm-5">
                     <div class="mt-4">
                       <button class="btn btn-primary shadow-0" type="button">Open</button>
