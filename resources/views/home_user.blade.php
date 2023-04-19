152ced4a92d61dd0b9749998fe1dc167aafc54a2
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-19 17:43:46 +0800

    slay or not slay

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -73,10 +73,10 @@
             CSWD Mental Health Web Application</h3>
         </div>
         <!--searchbar-->
-        <div class="form-group has-search">
+        <!--<div class="form-group has-search">
             <span class="fa fa-search form-control-feedback"></span>
             <input type="text" class="form-control" placeholder="Search">
-        </div>
+        </div>-->
     <ul class="list-unstyled components">
         <!--<p>Features</p>-->
         <li class="active">
