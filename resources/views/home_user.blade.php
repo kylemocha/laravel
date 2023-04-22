ab2735448c6f3b7cc6987425a2d5ec5eec22829f
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-22 12:30:07 +0800

    dk

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -472,16 +472,7 @@
         <span></span>
        </button>
        <h2 style="font-weight: bold;">Messages</h2>
-       @include('messenger.partials.flash')
-
-        @each('messenger.partials.thread', $threads, 'thread', 'messenger.partials.no-threads')
-
-        <div class="col-md-6">
-          <h1>{{ $thread->subject }}</h1>
-          @each('messenger.partials.messages', $thread->messages, 'message')
-  
-          @include('messenger.partials.form-message')
-      </div>
+       
       
     </div>
     <!---->    
