c592517f5cc069bac0d06031227de6373c6478b8
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-23 12:46:47 +0800

    idk

--- resources/views/therapist.blade.php
+++ resources/views/therapist.blade.php
@@ -248,8 +248,6 @@
         <div class="container">
           <div class="row">
               <div class="col-12">
-
-                  <h3 class="text-center mt-5">Calendar</h3>
                   <div class="col-md-11 offset-1 mt-5 mb-5">
   
                       <div id="calendar">
