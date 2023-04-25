064c7e588888d9bc22b5b8c62c55ef94684def63
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-25 18:42:46 +0800

    idk

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,7 +1,8 @@
 
 <h1>Create a new message</h1>
     <form action="{{ route('messages.store') }}" method="POST">
-        @csrf  
+        @csrf
+        @method('PUT')
 
         <div class="col-md-6">
             <!-- Subject Form Input -->
