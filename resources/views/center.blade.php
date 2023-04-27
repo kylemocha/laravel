74df25383d495977d88326407c16ffcef913c18d
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-04-27 18:14:26 +0800

    idk

--- resources/views/center.blade.php
+++ resources/views/center.blade.php
@@ -50,7 +50,109 @@
         </nav>
     </div>
 
+    <div class="wrapper">
+    <!--Carousel wrapper -->
+    <h1 style="text-align: center; font-weight: bold; font-size: 28px;">Our Centers</h1>
+    <!--Carousel Wrapper-->
+   <div id="carousel-example-2" class="carousel slide carousel-fade z-depth-1-half" data-ride="carousel">
+   <!--Indicators-->
+   <ol class="carousel-indicators">
+     <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+     <li data-target="#carousel-example-2" data-slide-to="1"></li>
+     <li data-target="#carousel-example-2" data-slide-to="2"></li>
+   </ol>
+   <!--/.Indicators-->
+   <!--Slides-->
+   <div class="carousel-inner" role="listbox">
+     <div class="carousel-item active">
+       <div class="view">
+         <img class="d-block w-100" src="https://i.ibb.co/gTYHBZB/Beige-and-Dark-Brown-Curated-Editorial-General-Photography-Photography-Website.png" alt="First slide">
+         <div class="mask rgba-black-light"></div>
+       </div>
+       <div class="carousel-caption">
+         <h3 class="h3-responsive"  style="color: #006a89; text-shadow: 2px 2px 2px #ABCDEF; font-weight: bold;">BAHAY PAG-ASA FOR GIRLS</h3>
+         <!--<p style="color: #006a89;">First text</p>-->
+         <li class="list-inline-item">
+           <a href="#" class="btn btn-primary d-flex justify-content-center d-md-table mx-auto">Learn More</a>
+         </li>
+       </div>
+     </div>
+     <div class="carousel-item">
+       <!--Mask color-->
+       <div class="view">
+         <img class="d-block w-100" src="https://i.ibb.co/DVkx2hL/Beige-and-Dark-Brown-Curated-Editorial-General-Photography-Photography-Website-1.png" alt="Second slide">
+         <div class="mask rgba-black-light"></div>
+       </div>
+       <div class="carousel-caption">
+         <h3 class="h3-responsive"  style="color: #006a89; text-shadow: 2px 2px 2px #ABCDEF; font-weight: bold;">BAHAY PAG-ASA: TAHANAN NG KABATAAN</h3>
+          <!--<p style="color: #006a89;">Secondary text</p>-->
+         <li class="list-inline-item">
+           <a href="#" class="btn btn-primary d-flex justify-content-center d-md-table mx-auto">Learn More</a>
+         </li>
+       </div>
+     </div>
+     <div class="carousel-item">
+       <!--Mask color-->
+       <div class="view">
+         <img class="d-block w-100" src="https://i.ibb.co/vZM6Y54/Beige-and-Dark-Brown-Curated-Editorial-General-Photography-Photography-Website-2.png" alt="Third slide">
+         <div class="mask rgba-black-light"></div>
+       </div>
+       <div class="carousel-caption">
+         <h3 class="h3-responsive"  style="color: #006a89; text-shadow: 2px 2px 2px #ABCDEF; font-weight: bold;">BOYS TOWN OF CDO</h3>
+         <!--<p style="color: #006a89;">Third text</p>-->
+         <li class="list-inline-item">
+           <a href="#" class="btn btn-primary d-flex justify-content-center d-md-table mx-auto">Learn More</a>
+         </li>
+       </div>
+     </div>
+     <div class="carousel-item">
+        <!--Mask color-->
+        <div class="view">
+          <img class="d-block w-100" src="https://i.ibb.co/zFZFTpp/Beige-and-Dark-Brown-Curated-Editorial-General-Photography-Photography-Website-3.png" alt=" Fourth slide">
+          <div class="mask rgba-black-light"></div>
+        </div>
+        <div class="carousel-caption">
+          <h3 class="h3-responsive"  style="color: #006a89; text-shadow: 2px 2px 2px #ABCDEF; font-weight: bold;">GOLDEN HOME CARE FOR THE ELDERLY</h3>
+          <!--<p style="color: #006a89;">Third text</p>-->
+          <li class="list-inline-item">
+            <a href="#" class="btn btn-primary d-flex justify-content-center d-md-table mx-auto">Learn More</a>
+          </li>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <!--Mask color-->
+        <div class="view">
+          <img class="d-block w-100" src="https://i.ibb.co/3kyKdfM/Beige-and-Dark-Brown-Curated-Editorial-General-Photography-Photography-Website-4.png" alt=" Fifth slide">
+          <div class="mask rgba-black-light"></div>
+        </div>
+        <div class="carousel-caption">
+          <h3 class="h3-responsive"  style="color: #006a89; text-shadow: 2px 2px 2px #ABCDEF; font-weight: bold;">SOCIAL DEVELOPMENT CENTER - HOME CARE FOR GIRLS</h3>
+          <!--<p style="color: #006a89;">Third text</p>-->
+          <li class="list-inline-item">
+            <a href="#" class="btn btn-primary d-flex justify-content-center d-md-table mx-auto">Learn More</a>
+          </li>
+        </div>
+      </div>
+   </div>
+   <!--/.Slides-->
+   <!--Controls-->
+   <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+     <span class="sr-only">Previous</span>
+   </a>
+   <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+     <span class="sr-only">Next</span>
+   </a>
+   <!--/.Controls-->
+ </div>
+ <!--/.Carousel Wrapper-->
+</div>
 
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </body>
 </html>
