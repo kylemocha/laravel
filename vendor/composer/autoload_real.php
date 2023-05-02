c3a881f8d39fddb223eadaaeb5dcc04ec107bc0a
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-05-02 10:25:41 +0800

    idk

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit475d29db86bb4184874fbeb04040646c
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit475d29db86bb4184874fbeb04040646c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit475d29db86bb4184874fbeb04040646c', 'loadClassLoader'));
