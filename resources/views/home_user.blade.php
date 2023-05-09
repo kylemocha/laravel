32637784af437b44dd3dcd0cf52122b8d7f25a95
Kyle Lamocha <20190017027@my.xu.edu.ph>
2023-05-09 09:58:22 +0800

    idk

--- resources/views/home_user.blade.php
+++ resources/views/home_user.blade.php
@@ -395,64 +395,6 @@
           });
         </script>
 
-      <br>
-      <h3 style="font-weight: bold;">Juan Dela Cruz Schedule's</h3>
-
-      <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="col-md-11 offset-1 mt-5 mb-5">
-
-                    <div id="calendar1">
-                    </div>
-            </div>
-         </div>
-      </div>
-    </div>
-
-    <script>
-      $(document).ready(function() {
-        var booking = @json($rivers);
-            $('#calendar1').fullCalendar({
-              header: {
-                    left: 'prev, next today',
-                    center: 'title',
-                    right: 'month, agendaWeek, agendaDay',
-              },
-              rivers : booking   
-            })
-        });
-      </script>
-
-    <br>
-    <h3 style="font-weight: bold;">Rose Lee Schedule's</h3>
-
-    <div class="container">
-      <div class="row">
-          <div class="col-12">
-              <div class="col-md-11 offset-1 mt-5 mb-5">
-
-                  <div id="calendar2">
-                  </div>
-          </div>
-      </div>
-    </div>
-    </div>
-
-    <script>
-    $(document).ready(function() {
-      var booking = @json($roses);
-          $('#calendar2').fullCalendar({
-            header: {
-                  left: 'prev, next today',
-                  center: 'title',
-                  right: 'month, agendaWeek, agendaDay',
-            }, 
-            roses : booking    
-          })
-      });
-    </script>
-
    
    
      <!--last div-->
